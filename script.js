76bc1a163403cd83f4228845b407b50782d948a2
Wojtek <wojciech.progr@gmail.com>
2023-11-04 21:12:19 +0100

    Listing and downloading elements, listening on click at start button

---
+++ script.js
@@ -0,0 +1,18 @@
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const historyBtn = document.querySelector('.history');
+const stopwatch = document.querySelector('.stopwatch');
+const time = document.querySelector('.time');
+const timeList = document.querySelector('.time-list');
+
+const infoBtn = document.querySelector('.info');
+const modalShadow = document.querySelector('.modal-shadow');
+const closeModalBin = document.querySelector('.close');
+
+const handleStart = () => {
+
+}
+
+startBtn.addEventListener('click', handleStart);
