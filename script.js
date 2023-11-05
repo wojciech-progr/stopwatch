cd3107e8706f34cb3ad98c384b4ca0f8509f0f09
Wojtek <wojciech.progr@gmail.com>
2023-11-05 01:36:08 +0100

    Counting and showing time after click on start button

--- script.js
+++ script.js
@@ -11,7 +11,30 @@ const infoBtn = document.querySelector('.info');
 const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBin = document.querySelector('.close');
 
+let countTime;
+let minutes = 0;
+let seconds = 0;
+
 const handleStart = () => {
+
+    countTime = setInterval(() => {
+
+        if (seconds < 9) {
+            // Add 1 second
+            seconds++;
+            stopwatch.textContent = `${minutes}:0${seconds}`;
+        } else if (seconds >= 9 && seconds < 59) {
+            // Add 10 seconds
+            seconds++;
+            stopwatch.textContent = `${minutes}:${seconds}`;
+        } else {
+            // Add 1 minute
+            minutes++;
+            seconds = 0;
+            stopwatch.textContent = `${minutes}:00`;
+        }
+
+    }, 100);
 
 }
 
